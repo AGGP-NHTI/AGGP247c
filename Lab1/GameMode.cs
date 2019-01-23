acc09d35c1e526c901a13a15e76149d4c6a80e64
Grash <grashuriza@gmail.com>
2019-01-22 20:10:28 -0500

    Added GameMode.cs

---
+++ Lab1/GameMode.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LineDraw;
+using Microsoft.Xna.Framework;
+
+namespace Lecture247
+{
+    class GameMode : GameApp
+    {
+        public GameMode() : base()
+        {
+
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            // do other Code after... 
+        }
+
+        protected override void LoadContent()
+        {
+            base.LoadContent();
+        }
+
+        protected override void GameUpdate(GameTime gameTime)
+        {
+           
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(clearColor);
+
+            base.Draw(gameTime);
+        }
+
+
+    }
+}
