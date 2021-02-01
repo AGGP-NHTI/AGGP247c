f1bde5fd05add762273e6a37980626185a1a0e4e
Grash <grashuriza@gmail.com>
2021-02-01 15:13:45 -0500

    Updated Lab 2 Materials

---
+++ Labs - Unity/Lab2/Code/DrawingObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Drawing.Glint;
+
+public class DrawingObject : MonoBehaviour
+{
+    public bool PerformDraw = true;
+    public float Roation = 0;
+    public Vector3 Scale = Vector3.zero;
+    public Vector3 Location = Vector3.zero;
+    public List<ICommandInstruction> Lines = new List<ICommandInstruction>();
+
+    public void Start()
+    {
+        Initalize(); 
+    }
+
+    public virtual void Initalize()
+    {
+    }
+
+    public virtual void Update()
+    {
+
+       if (PerformDraw)
+        {
+            Draw(); 
+        }
+    }
+    public virtual void Draw()
+    {
+        if (Lines.Count != 0)
+        {
+            for (int i = 0; i < Lines.Count; i++)
+            {
+                Lines[i].Draw(Location);
+            }
+        }
+    }
+}
